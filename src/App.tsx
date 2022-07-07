310dd7894196beeb3c2b714af577877e9b98ad29
ooheek <vkdlfl0224@gmail.com>
2022-07-07 20:43:21 +0900

    컴포넌트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AwardItem from './components/AwardItem'
+import AwardsContainer from './components/AwardsContainer'
+import ContentLogo from './components/ContentLogo'
+import MetricItem from './components/MetricItem'
+import MetricsContainer from './components/MetricsContainer'
+import Wrapper from './components/Wrapper'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Wrapper>
+      <ContentLogo label="2021년 12월 기준" />
+      <MetricsContainer>
+        <MetricItem num={700} unit="만 명" label="여행자" />
+        <MetricItem num={100} unit="만 개" label="여행 리뷰" />
+        <MetricItem num={470} unit="만 개" label="여행 일정" />
+      </MetricsContainer>
+      <AwardsContainer>
+        <AwardItem
+          imageUrl="/images/play-store2x.png"
+          labelArr={['2018 구글 플레이스토어', '올해의 앱 최우수상 수상']}
+        />
+        <AwardItem
+          imageUrl="/images/badge-apple4x.png"
+          labelArr={['2018 애플 앱스토어', '오늘의 여행앱 선정']}
+        />
+      </AwardsContainer>
+    </Wrapper>
+  )
 }
 
-export default App;
+export default App
