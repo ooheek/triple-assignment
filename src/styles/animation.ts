68a3a0d09cb610f26754b13f75ef454af10990f1
ooheek <vkdlfl0224@gmail.com>
2022-07-09 16:48:23 +0900

    styles 폴더 추가 및 fadeIn 애니메이션 코드 작성

---
+++ src/styles/animation.ts
@@ -0,0 +1,16 @@
+import { keyframes } from 'styled-components'
+
+// 위로 살짝 떠오르는 애니메이션
+// 투명도 조절 애니메이션
+export const fadeInAppear = keyframes`
+from {
+    opacity: 0;
+    transform: translateY(10%);
+  }
+to {
+    opacity: 1;
+    transform: translateY(0);
+}
+`
+
+export type DurationAndDelayType = { delay: number; duration: number }
